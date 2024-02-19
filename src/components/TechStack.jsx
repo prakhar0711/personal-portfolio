e3c33bb19d3a540dafa242d224708d2f587f4965
Prakhar Pratyush <prakhar.pratyush8010@gmail.com>
2024-02-19 23:44:32 +0530

    techstack

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -11,39 +11,41 @@ import django from "../assets/icons/techstack_icons/django.png";
 import spring from "../assets/icons/techstack_icons/springboot.png";
 import tailwind from "../assets/icons/techstack_icons/tailwind.png";
 import react from "../assets/icons/techstack_icons/react.png";
-
+import "../assets/css/techstack.css";
 function TechStack() {
   return (
     <>
-      <div className="w-full p-10 sm:p-20 bg-[#0e131e]">
-        <div className="px-10 sm:px-20 flex lg:justify-center sm:justify-center text-2xl sm:text-4xl">
-          <img src={techstackicon} alt="techstack" className="w-20 sm:w-40" />
-        </div>
-        <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-10 sm:p-20">
-          <div className="flex justify-center">
-            <TechStackCard
-              title="Languages"
-              description="C, C++, Python, Rust, JavaScript, HTML, CSS"
-              img1={cpp}
-              img2={c}
-              img3={py}
-              img4={rust}
-              img5={js}
-              img6={html}
-              img7={css}
-            />
+      <div className="tech-stack-container">
+        <div className="w-full p-10 sm:p-20 bg-[#0e131e]">
+          <div className="px-10 sm:px-20 flex lg:justify-center sm:justify-center text-2xl sm:text-4xl">
+            <img src={techstackicon} alt="techstack" className="w-20 sm:w-40" />
           </div>
-          <div className="flex justify-center">
-            <TechStackCard
-              title="Frameworks"
-              description="Django, SpringBoot, Tailwind CSS"
-              img1={django}
-              img2={spring}
-              img3={tailwind}
-            />
-          </div>
-          <div className="flex justify-center">
-            <TechStackCard title="Library" description="React" img1={react} />
+          <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-10 sm:p-20">
+            <div className="flex justify-center">
+              <TechStackCard
+                title="Languages"
+                description="C, C++, Python, Rust, JavaScript, HTML, CSS"
+                img1={cpp}
+                img2={c}
+                img3={py}
+                img4={rust}
+                img5={js}
+                img6={html}
+                img7={css}
+              />
+            </div>
+            <div className="flex justify-center">
+              <TechStackCard
+                title="Frameworks"
+                description="Django, SpringBoot, Tailwind CSS"
+                img1={django}
+                img2={spring}
+                img3={tailwind}
+              />
+            </div>
+            <div className="flex justify-center">
+              <TechStackCard title="Library" description="React" img1={react} />
+            </div>
           </div>
         </div>
       </div>
